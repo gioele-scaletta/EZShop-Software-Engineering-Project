717ad7271d9f1f61105d16e2d18fa20a9659477f
Giuseppe D'Andrea <s287584@studenti.polito.it>
2021-05-05 16:53:18 +0200

    feat: add BalanceOperationImpl class

---
+++ src/main/java/it/polito/ezshop/data/BalanceOperationImpl.java
@@ -0,0 +1,51 @@
+package it.polito.ezshop.data;
+
+import java.time.LocalDate;
+
+public class BalanceOperationImpl implements BalanceOperation {
+    private int balanceId;
+    private LocalDate date;
+    private double money;
+    private String type;
+
+
+    @Override
+    public int getBalanceId() {
+        return this.balanceId;
+    }
+
+    @Override
+    public void setBalanceId(int balanceId) {
+        this.balanceId = balanceId;
+    }
+
+    @Override
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    @Override
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public double getMoney() {
+        return this.money;
+    }
+
+    @Override
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String getType() {
+        return this.type;
+    }
+
+    @Override
+    public void setType(String type) {
+        this.type = type;
+    }
+}
