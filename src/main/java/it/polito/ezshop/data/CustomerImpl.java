4a831f0dd363f36b90323d42ddd219bc12d9719b
Jose Antonio Antona Diaz <s286490@studenti.polito.it>
2021-05-07 10:55:44 +0000

    Update CustomerImpl.java

--- src/main/java/it/polito/ezshop/data/CustomerImpl.java
+++ src/main/java/it/polito/ezshop/data/CustomerImpl.java
@@ -2,47 +2,64 @@ package it.polito.ezshop.data;
 
 public class CustomerImpl implements Customer{
 
+    private String customerName;
+    private String customerCard;
+    private Integer id;
+    private Integer points;
 
+    public CustomerImpl(String customerName, Integer id){
+
+        // Customer name
+        this.customerName = customerName;
+
+        // A fidelity card has not been attached to the client
+        this.customerCard = "";
+
+        // Customer id
+        this.id = id;
+
+        // The customer starts with 0 points on the fidelity card
+        this.points = 0; 
+    }
 
     @Override
     public String getCustomerName() {
-        return null;
+        return this.customerName;
     }
 
     @Override
     public void setCustomerName(String customerName) {
-
+        this.customerName = customerName;
     }
 
     @Override
     public String getCustomerCard() {
-        return null;
+        return this.customerCard;
     }
 
     @Override
     public void setCustomerCard(String customerCard) {
-
+        this.customerCard = customerCard;
     }
 
     @Override
     public Integer getId() {
-        return null;
+        return this.id;
     }
 
     @Override
     public void setId(Integer id) {
-
+        this.id = id;
     }
 
     @Override
     public Integer getPoints() {
-        return null;
+        return this.points;
     }
 
     @Override
     public void setPoints(Integer points) {
-
+        this.points = points;
     }
-
 
 }
